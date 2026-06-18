84ef300dc7ccc16cd8c050714804c8c260748063
mmohsin-786786 <mmohsin.786786@gmail.com>
2026-06-18 03:36:36 -0400

    Change in server file

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
+"""
+This server file has a flash application to run sentimental analysis
+"""
 # Import the relevant libraries and functions
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 # Initiate the Flask app
@@ -7,32 +10,33 @@ app = Flask("Emotion Detector")
 
 # Define the function
 @app.route("/emotionDetector")
-def sent_analyzer(): 
-    
-    # Retrieve the text to analyze from the request arguments 
+def sent_analyzer():
+    """
+    # Retrieve the text to analyze from the request arguments
+    """
     text_to_analyze = request.args.get('textToAnalyze')
-    
     if not text_to_analyze or text_to_analyze.strip() == "":
-       return "Invalid text! Please try again!."
-    
-    # Pass the text to the sentiment_analyzer function and store the response 
+        return "Invalid text! Please try again!."
+    # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
-
     # Extract the value of dominant_emotion
     dominant_emotion = response['dominant_emotion']
-    
     # Check if the label is None, indicating an error or invalid input
     if dominant_emotion is None:
         return "Invalid input! Try again."
-    else:
-    #Return a formatted string with the sentiment label and score
-        return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
-
+    return (
+        f"For the given statement, the system response is 'anger': "
+        f"{response['anger']}, 'disgust': {response['disgust']}, "
+        f"'fear': {response['fear']}, 'joy': {response['joy']} and "
+        f"'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    )
 # Render the HTML template using render_index_page
 @app.route("/")
 def render_index_page():
+    """
+    To render the index.html file using render_template
+    """
     return render_template('index.html')
-
 # Run the application on localhost:5000
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
